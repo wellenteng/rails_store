8ff457bf14cc693158da3a0d5d705baffbd8a28c
Lex Teng <wellenteng@gmail.com>
2016-01-16 17:16:51 +0800

    購物車與session hash之間的轉換

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,8 +1,18 @@
  class Cart
   attr_reader :items
 
-  def initialize
-    @items = []
+  def self.build_from_hash hash
+    items = hash["cart"]["items"].map do |item_data|
+      CartItem.new item_data["product_id"], item_data["quantity"]
+    end
+
+    new items
+  end
+
+
+
+  def initialize items = []
+    @items = items
   end
 
   def add_item product_id
@@ -12,6 +22,20 @@
     else
       @items << CartItem.new(product_id)
     end
+  end
+
+  def serialize
+    items = @items.map do |item|
+      {
+        "product_id" => item.product_id, "quantity" => item.quantity
+      }
+    end
+
+    {
+      "cart" => {
+        "items" => items
+      }
+    }
   end
 
   def empty?
--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -24,4 +24,28 @@ class CartTest < MiniTest::Test
 
     assert_kind_of Product, cart.items.first.product
   end
+
+  def test_serializes_to_hash
+    cart = Cart.new
+    cart.add_item 1
+
+    assert_equal cart.serialize, session_hash
+  end
+
+  def test_builds_from_hash
+    cart = Cart.build_from_hash session_hash
+    assert_equal 1, cart.items.first.product_id
+  end
+
+  private
+
+  def session_hash
+    {
+      "cart" => {
+        "items" => [
+          { "product_id" => 1, "quantity" => 1 }
+        ]
+      }
+    }
+  end
 end
